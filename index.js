8a190b3c5b32a4f55024cb9c819760939c91ec2f
yazidmufid05 <144298019+yazidmufid05@users.noreply.github.com>
2023-11-06 10:44:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,143 @@
+class Ulul_azmi {
+    constructor() {
+      this.nama = "";
+      this.tugas = "";
+      this.sifat = "";
+      this.ujian = "";
+      this.umur = "";
+      this.mukjizat = "";
+    }
+  
+    mengajak(nama) {
+      console.log(nama + " mengajak orang2 untuk");
+    }
+  
+    menyampaikan(nama) {
+      console.log(nama + "tidak pernah berhenti menyampaikan");
+    }
+  
+    berkontrak(nama) {
+      console.log(nama + "Menerima perjanjian dan juga wasiat dari ALLAH");
+    }
+  
+    mengajarkan(nama) {
+      console.log(nama + "Mengajarkan ajaran islam yang sudah ALLAH wahyukan");
+    }
+  
+    memperoleh(nama) {
+      console.log(nama + "Memperoleh");
+    }
+  
+    berdoa(nama) {
+      console.log(
+        nama +
+          "Selalu berdoa kepada ALLAH agar diberikan hidayah dan kekuatan untuk menghadapi kaum"
+      );
+    }
+  
+    berbicara(nama) {
+      console.log(nama + "selalu berbicara jujur");
+    }
+  
+    melakukan(nama) {
+      console.log(nama + "Melakukan kebaikan");
+    }
+  }
+  
+  
+  
+  NabiNuh = new Ulul_azmi();
+  let nama = (NabiNuh.nama = "Nabi Nuh Alaihissalam");
+  let tugas = (NabiNuh.tugas =
+    "Menyeru kaum nya untuk tidak meminta kepada patung orang sholeh dan menyadarkan bahwa ALLAH sesungguhnya tuhan yang maha pemberi rizki");
+  let sifat = (NabiNuh.sifat = "Sabar");
+  let ujian = (NabiNuh.ujian = "Anak dan istrinya menentang ajaran islam");
+  let umur = (NabiNuh.umur = "350 Tahun");
+  let keistimewaan = (NabiNuh.mukjizat = "Membuat kapal besar di atas gunung");
+  let ajak = (NabiNuh.mengajak =
+    "Sedang mengajak orang2 untuk kembali kepada ajaran islam");
+  let wahyu = (NabiNuh.menyampaikan =
+    "Sedang menyampaikan bahwa akan datang banjir bandang");
+  let wasiat = (NabiNuh.berkontrak = "Sedang menerima wasiat dari ALLAH");
+  let melakukan = (NabiNuh.melakukan =
+    "Sedang membuat kapal yang sangat besar di atas gunung");
+  let berdoa = (NabiNuh.berdoa =
+    "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah");
+  let berbicara = (NabiNuh.berbicara =
+    "Sedang berbicara kepada kaumnya yang sholeh bahwa akan ada banjir besar");
+  
+  NabiIbrahim = new Ulul_azmi();
+  nama = NabiIbrahim.nama = "Nabi Ibrahim Alaihissalam";
+  tugas = NabiIbrahim.tugas =
+    "Melarang kaumnya yang menyembah berhala raja namrud";
+  sifat = NabiIbrahim.sifat = "Sabar";
+  ujian = NabiIbrahim.ujian = "Menghadapi raja namrud";
+  umur = NabiIbrahim.umur = "86 Tahun";
+  keistimewaan = NabiIbrahim.mukjizat =
+    "Tidak terbakar ketika raja namrud membakarnya";
+  ajak = NabiIbrahim.mengajak =
+    "Sedang mengajak orang2 untuk beriman kepada ALLAH";
+  wahyu = NabiIbrahim.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+  wasiat = NabiIbrahim.berkontrak = "Sedang menerima wasiat dari ALLAH";
+  berdoa = NabiIbrahim.berdoa =
+    "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+  berbicara = NabiIbrahim.berbicara =
+    "Sedang berbicara kepada berhala raja namrud";
+  melakukan = NabiIbrahim.melakukan =
+    "Sedang memenggal kepala berhalanya raja namrud";
+  
+  NabiIMusa = new Ulul_azmi();
+  nama = NabiIMusa.nama = "Nabi Musa Alaihissalam";
+  tugas = NabiIMusa.tugas =
+    "Menegaskan bahwa tiada tuhan yang hak disembah selain ALLAH bukan firaun";
+  sifat = NabiIMusa.sifat = "Sabar";
+  ujian = NabiIMusa.ujian = "Menghadapi Firaun";
+  umur = NabiIMusa.umur = "40 Tahun";
+  keistimewaan = NabiIMusa.mukjizat = "Membelah lautan";
+  ajak = NabiIMusa.mengajak =
+    "Sedang mengajak orang2 untuk meyakini bahwa ALLAH sesungguhnya tuhan";
+  wahyu = NabiIMusa.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+  wasiat = NabiIMusa.berkontrak = "Sedang menerima wasiat dari ALLAH";
+  berdoa = NabiIMusa.berdoa =
+    "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+  berbicara = NabiIMusa.berbicara = "Sedang berbicara kepada raja firaun";
+  melakukan = NabiIMusa.melakukan = "Sedang membelah lautan";
+  
+  NabiIsa = new Ulul_azmi();
+  nama = NabiIsa.nama = "Nabi Isa Alaihissalam";
+  tugas = NabiIsa.tugas = "Mengajarkan ajaran islam";
+  sifat = NabiIsa.sifat = "Sabar";
+  ujian = NabiIsa.ujian = "Memiliki murid dan umat yang berkhianat";
+  umur = NabiIsa.umur = "30 Tahun";
+  keistimewaan = NabiIsa.mukjizat = "Kitab Injil";
+  ajak = NabiIsa.mengajak = "Sedang mengajak orang2 untuk beriman kepada ALLAH";
+  wahyu = NabiIsa.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+  wasiat = NabiIsa.berkontrak = "Sedang menerima wasiat dari ALLAH";
+  berdoa = NabiIsa.berdoa =
+    "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+  berbicara = NabiIsa.berbicara =
+    "Sedang berbicara kepada kaum yahudi ketika masih bayi";
+  melakukan = NabiIsa.melakukan = "Sedang memnghidupkan orang mati";
+  
+  NabiMuhammad = new Ulul_azmi();
+  nama = NabiMuhammad.nama = "Nabi Muhammad Shalallahu Alaihi Wasallam";
+  tugas = NabiMuhammad.tugas = "Mengajarkan ajaran islam";
+  sifat = NabiMuhammad.sifat = "Sabar";
+  ujian = NabiMuhammad.ujian = "Yatim piatu ditinggalkan orang-orang tersayang kesusahan di makkah kesusahan di madinah Memiliki paman dan bibi yang memusuhi nabi";
+  umur = NabiMuhammad.umur = "40 Tahun";
+  keistimewaan = NabiMuhammad.mukjizat = "isra miraj Kitab Al-Quran";
+  ajak = NabiMuhammad.mengajak =
+    "Sedang mengajak orang2 untuk menunaikan sholat,zakat,puasa,dan haji";
+  wahyu = NabiMuhammad.menyampaikan = "Sedang menyampaikan wahyu ALLAH";
+  wasiat = NabiMuhammad.berkontrak = "Sedang menerima wasiat dari ALLAH";
+  berdoa = NabiMuhammad.berdoa =
+    "Sedang berdoa kepada ALLAH agar kaumnya diberikan petunjuk dan hidayah";
+  berbicara = NabiMuhammad.berbicara = "Sedang berbicara kepada kaumnya";
+  melakukan = NabiMuhammad.melakukan = "Sedang melakukan isra miraj";
+  
+  const para_nabi = [NabiMuhammad, NabiIsa, NabiIMusa, NabiNuh, NabiIbrahim,];
+  
+  for (nabi of para_nabi) {
+    console.log(nabi);
+  }
+  
